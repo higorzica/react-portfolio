3dc4987b5703fbf162b0a855981229a93dc3ae78
Higor Zica <higorbhe@gmail.com>
2022-10-19 12:08:11 -0300

    adicionando site academic
    2 fotos por item no portfolio.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,11 @@ import IMG1 from '../../assets/barbearia.png'
 import IMG2 from '../../assets/pizzaria.png'
 import IMG3 from '../../assets/salao.png'
 import IMG4 from '../../assets/profmargaux.jpeg'
-import IMG5 from '../../assets/nubank-proj.jpeg'
+import IMG5 from '../../assets/profmargauxpacotes.png'
+import IMG6 from '../../assets/nubank-proj.jpeg'
+import IMG7 from '../../assets/nubank-mockup.png'
+import IMG8 from '../../assets/AcademicSite.png'
+import IMG9 from '../../assets/AcademicSuporte.png'
 
 
 const Portfolio = () => {
@@ -52,6 +56,10 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG4} alt="portfolio1" />
           </div>
+          <br />
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="portfolio1" />
+          </div>
           <h3>Site Persolizável com Sistema de Gestão para Aulas Particulares / Escola de Idiomas. </h3>
           <h5>Responsivo para Mobile</h5>
           <div className="portfolio__item-cta">
@@ -61,13 +69,32 @@ const Portfolio = () => {
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG5} alt="portfolio1" />
+            <img src={IMG6} alt="portfolio1" />
+          </div>
+          <br />
+          <div className="portfolio__item-image">
+            <img src={IMG7} alt="portfolio1" />
           </div>
           <h3>Site Nubank, reformulação aplicando SASS, Conceitos de Design 3d, UX/UI compativel com a cultura da organização. </h3>
           <h5>Responsivo para Mobile</h5>
           <div className="portfolio__item-cta">
             <a href="https://github.com/" target='_blank' className='btn'>GitHub</a>
             <a href="https://dribbble.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG8} alt="portfolio1" />
+          </div>
+          <br />
+          <div className="portfolio__item-image">
+            <img src={IMG9} alt="portfolio1" />
+          </div>
+          <h3>Site para empresa de Software de Gerenciamento Escolar, tecnologia usadas HTML, SASS, JS, UX/UI compativel com a cultura da organização. </h3>
+          <h5>Responsivo para Mobile</h5>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" target='_blank' className='btn'>GitHub</a>
+            <a href="http://academic.profmargauxfrances.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
           </div>
         </article>
       </div>
